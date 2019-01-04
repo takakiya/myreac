06fb90a7ecc4b2c4cd6044ecaa19db15eefc443c
takakiya <takakiya@sea.plala.or.jp>
2019-01-04 14:09:15 +0900

    about props

--- src/App.js
+++ src/App.js
@@ -3,30 +3,32 @@
 
 import React from 'react';
 
-// class App extends Component {
-//  render() {
-//    return(
-//      <React.Fragment>
-//      <label htmlFor="bar">bar</label>
-//      <input type="text" onClick={() => { alert("hoge") }} />
-//      </React.Fragment>
-//      )
-//  }
-//}
 
 const App = () => {
+  
+  const profiles =[
+    {name: "Taro", age: 10},
+    {name: "Hanako", age: 8},
+    {name: "Noname"}
+  ]  
+  
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index )=> {
+          return <User name={profile.name} age={profile.age} kwy={profile.index} />
+        })
+      }
     </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!!</div>
+const User = (props) => {
+  return <div>Hi!!{props.name} and {props.age}</div>
+}
+
+User.defaultProps ={
+  age:1
 }
 
 export default App;
