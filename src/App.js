f89e6e833b248f4b844bb56f1dfd9a42395dac04
takakiya <takakiya28go@navy.plala.or.jp>
2025-01-30 13:32:22 +0900

    auto format

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
+import React, { Component } from 'react';
 
-
-
-import React, {Component} from 'react';
-
-const App = () => (<Counter></Counter>)
+const App = () => <Counter />;
 
 class Counter extends Component {
   constructor(props) {
-    super(props)
-    this.state = { count: 0}
+    super(props);
+    this.state = { count: 0 };
   }
 
-  handlePlusButton = () =>{
-    this.setState({count: this.state.count +1})
-  }
-  handleMinusButton = () =>{
-    this.setState({count: this.state.count -1})
-  }
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
-render() {
+  render() {
     return (
       <React.Fragment>
-      <div>count: {this.state.count}</div>
-      <button onClick={this.handlePlusButton} >+1</button>
-      <button onClick={this.handleMinusButton} >-1</button>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
       </React.Fragment>
-      )
-    }
+    );
+  }
 }
- 
+
 export default App;
